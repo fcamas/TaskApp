dc0a6a2c67abf58259de2ee3cdd3b45e0c0d4cd7
fcamas <fredycamasdev@gmail.com>
2024-04-15 14:01:00 -0400

    added navigation root

---
+++ lab-task-squirrel/Task Compose/TaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskViewController.swift
+//  lab-task-squirrel
+//
+//  Created by Fredy Camas on 4/26/24.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
