140900edca1139d424b6d774b522a45d85258b46
fcamas <fredycamasdev@gmail.com>
2024-05-01 21:42:29 -0400

    added generic init at cell

--- lab-task-squirrel/Views/ItemCell.swift
+++ lab-task-squirrel/Views/ItemCell.swift
@@ -19,6 +19,15 @@ class ItemCell: UITableViewCell {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        setupView()
+    }
     
     private func setupView() {
         addSubview(titleLabel)
