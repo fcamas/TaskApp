d6638c49ee68664c5cbc8e91617c27a5cfa2af40
fcamas <fredycamasdev@gmail.com>
2024-05-04 14:04:00 -0400

    fixed views

--- lab-task-squirrel/Views/ItemCell.swift
+++ lab-task-squirrel/Views/ItemCell.swift
@@ -25,8 +25,8 @@ class ItemCell: UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
+       
         fatalError("init(coder:) has not been implemented")
-        setupView()
     }
     
     private func setupView() {
